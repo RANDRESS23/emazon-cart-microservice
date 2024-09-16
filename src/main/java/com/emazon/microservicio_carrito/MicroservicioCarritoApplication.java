1c5eb3a6c4036becea2ffb5fd9af529b617ef687
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-16 14:46:08 -0500

    feat: add base project

---
+++ src/main/java/com/emazon/microservicio_carrito/MicroservicioCarritoApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.microservicio_carrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCarritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioCarritoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emazon/microservicio_carrito/MicroservicioCarritoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emazon.microservicio_carrito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioCarritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
