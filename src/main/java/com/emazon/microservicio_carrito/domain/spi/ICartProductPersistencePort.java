c25024fb7212afe54d137513f35fad3309fa3894
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-21 19:29:40 -0500

    feat: add remove cart product method in the persistence port

--- src/main/java/com/emazon/microservicio_carrito/domain/spi/ICartProductPersistencePort.java
+++ src/main/java/com/emazon/microservicio_carrito/domain/spi/ICartProductPersistencePort.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public interface ICartProductPersistencePort {
     CartProduct saveCartProduct(CartProduct cartProduct);
+    void removeCartProduct(CartProduct cartProduct);
     List<CartProduct> getAllProducts(Long cartId);
 }
